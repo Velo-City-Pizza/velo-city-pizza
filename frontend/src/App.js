dc31fc1e77eec7fbcb6a690a26b9328c356478e7
Lance Chong-Kit <lchongki@ucsc.edu>
2023-02-28 17:20:50 -0800

    Init frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <div className = "pages">
+          <Routes>
+            <Route
+              path="/"
+              element={}
+              />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
