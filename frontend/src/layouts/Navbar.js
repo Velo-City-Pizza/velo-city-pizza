e2a3e8a053c8ce5b3c20e2b08def5167da75e460
Lance Chong-Kit <lchongki@ucsc.edu>
2023-03-05 03:55:22 -0800

    Finish navbar

--- frontend/src/layouts/Navbar.js
+++ frontend/src/layouts/Navbar.js
@@ -1,17 +1,28 @@
+import { useState } from 'react'
 
 const Navbar = () => {
     const logo = require('../assets/img/velo_city_logo.png');
+    const [expandMenu, setExpandMenu] = useState(false)
+
+    function expandMobileMenu() {
+        setExpandMenu(!expandMenu)
+    }
+
     return (
         <nav className="navbar">
-            <a class="nav-logo" href="/"><img src={logo} /></a>
-            <ul>
+            <a className="nav-logo" href="/"><img src={logo} /></a>
+            <a className ="nav-name" href="/">Velo City Pizza</a>
+            <div className="mobile-burger" onClick={expandMobileMenu}>
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
+            </div>
+            <ul className={`nav-links${expandMenu ? ' active' : ''}`}>
                 <li><a href="/">Home</a></li>
                 <li><a href="/menu">Menu</a></li>
                 <li><a href="/order_ahead">Order</a></li>
             </ul>
-            <div className="mobile-menu">
-                <p>Menu</p>
-            </div>
+            
         </nav>
 
     )
